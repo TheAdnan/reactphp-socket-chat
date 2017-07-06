a053d3dd1ed9457b3fa11b71fa60465e58d94251
TheAdnan <adnankicin92@gmail.com>
2017-07-06 11:57:06 +0200

    username when new user enters and leaves chat

--- ConnectionsPool.php
+++ ConnectionsPool.php
@@ -12,10 +12,19 @@
             $this->connections = new SplObjectStorage();
         }
 
+        protected function setConnectionData(ConnectionInterface $connection, $data){
+            $this->connections->offsetSet($connection, $data);
+        }
+
+        protected function getConnectionData(ConnectionInterface $connection){
+            $this->connections->offsetGet($connection);
+        }
+
         public function add(ConnectionInterface $connection){
-            $connection->write("Selam\n");
+            $connection->write("Unesi korisničko ime: ");
 
             $this->initEvents($connection);
+            $this->setConnectionData($connection, []);
             $this->connections->attach($connection);
 
             $this->sendAll("Novi korisnik je ušao u chat\n", $connection);
@@ -23,20 +32,35 @@
 
         protected function initEvents(ConnectionInterface $connection){
             $connection->on('data', function($data) use ($connection){
-                $this->sendAll($data, $connection);
+                $connectionData = $this->getConnectionData($connection);
+
+                if(!empty($connectionData)){
+                    $this->sendJoinMessage($data, $connection);
+                    return;
+                }
+
+                $name = $connectionData['name'];
+                $this->sendAll("$name: $data", $connection);
             });
 
             $connection->on('close', function() use ($connection){
-                $this->connections->detach($connection);
-                $this->sendAll("Korisnik je napustio chat\n", $connection);
+                $connectionData = $this->getConnectionData($connection);
+                $name = $connectionData['name'];
+                $this->connections->offsetUnset($connection);
+                $this->sendAll("Korisnik $name je napustio chat\n", $connection);
             });
+        }
+
+        protected function sendJoinMessage($name, $connection){
+            $name = str_replace(['\n', '\r'], " ", $name);
+            $this->setConnectionData($connection, ['name' => $name]);
+            $this->sendAll("Korisnik $name je ušao u chat\n", $connection);
         }
 
         protected function sendAll($text, ConnectionInterface $connection){
             foreach ($this->connections as $conn) {
                 if($conn !== $connection) $conn->write($text);
             }
-
         }
 
     }
